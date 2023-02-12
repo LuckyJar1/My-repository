4dbf6c0013d4e7e9c2481d8c5dc4d169572ee582
LuckyJar1 <registeren@yandex.ru>
2023-02-12 14:57:30 +0300

    first

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+
+#include <map>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru");
+    map<int, string> id =
+    { {10,"Иеруалимов Артём Сергеевич"},
+      {3,"Иван Иванов Иванушкин"},
+      {8,"Денис Денисович Денисов"},
+      {7,"Кирилл Кириллович Кириллов"},
+      {14,"ПОН ПОНОВИЧ ПОНОВ"}
+    };
+    int k = 0;
+    int i = 0;
+    cout << "Введите айди: ";
+    cin >> k;
+    for (auto pair : id)
+    {
+        if (k <= pair.first)
+        {
+            cout << "Id :" << pair.first << "  Fio :" << pair.second << endl;
+            i++;
+        }
+
+    }
+    cout << "Всего людей удв условию: " << i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
